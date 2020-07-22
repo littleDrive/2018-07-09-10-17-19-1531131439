b61569c7ab3620cf7f87f054c1c2d18f810ec38b
littleDrive <ZERO.OU@OOCL.COM>
2020-07-22 18:15:55 +0800

    [practice08][zero_ou]feature: update all class

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,35 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+//    设置班长
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
