fb09bbb752d755f958e655090bab3157fc547f80
littleDrive <ZERO.OU@OOCL.COM>
2020-07-22 16:09:36 +0800

    [practice06][zero_ou]feature: add the Teacher class which extends to Person

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    protected int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = -1;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (getKlass() == -1) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach Class " + getKlass() + ".";
+    }
 }
